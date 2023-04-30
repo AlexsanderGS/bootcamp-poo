c039a32abafc13ce083f2b526eaf175127df18b7
AlexsanderGS <alexsanderga@gmail.com>
2023-04-29 22:08:21 -0300

    Criada as classes bootcamp, devs e conteudo

--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -1,36 +1,20 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
+public class Curso extends Conteudo {
 	
-	private String titulo;
-	private String descricao;
 	private Integer cargaHoraria;
 	
-	public Curso() {
-		
+	public Curso() {	
+	}
+	
+	@Override
+	public double calcularXp() {
+		return XP_PADRAO * cargaHoraria;
 	}
 
 	public Curso(String titulo, String descricao, Integer cargaHoraria) {
 		super();
-		this.titulo = titulo;
-		this.descricao = descricao;
 		this.cargaHoraria = cargaHoraria;
-	}
-
-	public String getTitulo() {
-		return titulo;
-	}
-
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
 	}
 
 	public Integer getCargaHoraria() {
@@ -43,9 +27,12 @@ public class Curso {
 
 	@Override
 	public String toString() {
-		return "Curso [titulo=" + titulo + ", descricao=" + descricao + ", cargaHoraria=" + cargaHoraria + "]";
+		return "Curso [titulo=" 
+				+ getTitulo() 
+				+ ", descricao=" 
+				+ getDescricao() 
+				+ ", cargaHoraria=" 
+				+ cargaHoraria + "]";
 	}
 	
-	
-
 }
